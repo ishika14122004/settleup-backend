79c0f3acea487a75c6811530d2a71dd3c917c88e
Ishika Barnwal <167006718+ishika14122004@users.noreply.github.com>
2025-12-30 22:45:07 +0530

    Initial Spring Boot setup with health endpoint

---
+++ src/main/java/com/ishika/settleupbackend/SettleUpBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ishika.settleupbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettleUpBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettleUpBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Ishika/settleup_backend/SettleUpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ishika.settleup_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettleUpBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
